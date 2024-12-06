803690a979ead8b925964d61b733e72be54061d2
khoa.trandang2 <khoa.trandang2@di4l.vn>
2024-12-06 10:35:15 +0700

    add jenny dialogue

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:jenny/jenny.dart';
 import 'package:visual_novel_game/project_view_component.dart';
 
 void main() {
@@ -17,13 +19,24 @@ class JennyGame extends FlameGame {
   late Sprite boatBackgroundSprite;
   late Sprite girlSprite;
   late Sprite boySprite;
+  YarnProject yarnProject = YarnProject();
+  ProjectViewComponent projectViewComponent = ProjectViewComponent();
 
   @override
   FutureOr<void> onLoad() async {
     boatBackgroundSprite = await loadSprite('boat.webp');
     girlSprite = await loadSprite('girl.webp');
     boySprite = await loadSprite('boy.webp');
-    add(ProjectViewComponent());
+    String startDialogueData = await rootBundle.loadString(
+      'assets/yarn/start.yarn',
+    );
+    yarnProject.parse(startDialogueData);
+    var dialogueRunner = DialogueRunner(
+      yarnProject: yarnProject,
+      dialogueViews: [projectViewComponent],
+    );
+    dialogueRunner.startDialogue('Boat_Meeting');
+    add(projectViewComponent);
     return super.onLoad();
   }
 }
--- lib/project_view_component.dart
+++ lib/project_view_component.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
 import 'package:jenny/jenny.dart';
 import 'package:visual_novel_game/main.dart';
 
@@ -9,6 +11,8 @@ class ProjectViewComponent extends PositionComponent
   final background = SpriteComponent();
   final girl = SpriteComponent();
   final boy = SpriteComponent();
+  late final ButtonComponent forwardButtonComponent;
+  Completer<void> _fowardCompleter = Completer();
 
   @override
   FutureOr<void> onLoad() {
@@ -22,7 +26,35 @@ class ProjectViewComponent extends PositionComponent
       ..sprite = gameRef.boySprite
       ..size = Vector2(400, 600)
       ..position = Vector2(gameRef.size.x * .7, 0);
-    addAll([background, girl, boy]);
+    forwardButtonComponent = ButtonComponent(
+      button: PositionComponent(),
+      size: gameRef.size,
+      onPressed: () {
+        if (!_fowardCompleter.isCompleted) {
+          _fowardCompleter.complete();
+        }
+      },
+    );
+    addAll([
+      background,
+      girl,
+      boy,
+      forwardButtonComponent,
+    ]);
     return super.onLoad();
+  }
+
+  @override
+  FutureOr<bool> onLineStart(DialogueLine line) async {
+    _fowardCompleter = Completer();
+    await _advance(line);
+    return super.onLineStart(line);
+  }
+
+  Future<void> _advance(DialogueLine line) async {
+    var characterName = line.character?.name ?? '';
+    var dialogueLineText = '$characterName: ${line.text}';
+    debugPrint('debug: $dialogueLineText');
+    return _fowardCompleter.future;
   }
 }
